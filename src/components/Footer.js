c072e3be4275ab9ed91356e6bbaa1aaada3cb78d
unknown <benbushman98@gmail.com>
2022-11-05 10:09:08 -0600

    added more work for mobile responsiveness first design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="text-center fixed-bottom w-100 bg-light">
+    <div className='m-5'>
+      <footer className="text-center fixed-bottom w-100 bg-light">
         <p>Created and Designed by Benjamin Bushman | Copyright © 2022</p>
-    </footer>
+      </footer>
+    </div>
   )
 }
 
