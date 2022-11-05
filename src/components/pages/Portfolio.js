df3560f83e9048eb24023d460e034ce35a8ddab3
unknown <benbushman98@gmail.com>
2022-11-05 17:44:58 -0600

    added fixes for responsiveness on bigger screens

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,8 +3,10 @@ import React from 'react'
 
 const Portfolio = () => {
     return (
-        <div className="d-flex justify-content-center mx-5 px-5 flex-wrap">
-            <Project />
+        <div>
+            <div className='d-flex justify-content-center flex-wrap'>
+                <Project />
+            </div>
         </div>
     )
 }
