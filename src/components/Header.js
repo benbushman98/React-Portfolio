1942212b855c28d03ca746488b507e473ff43b8d
unknown <benbushman98@gmail.com>
2022-11-13 13:18:06 -0700

    added route

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,7 +34,7 @@ return (
   <header>
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <a className="navbar-brand text-white" href="/React-Portfolio">Benjamin Bushman</a>
+        <a className="navbar-brand text-white">Benjamin Bushman</a>
         <button className="navbar-toggler bg-white rounded-pill" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
