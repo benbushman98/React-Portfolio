f4b266235e0684bc6a9bd61551cc06ff5be2ee6a
unknown <benbushman98@gmail.com>
2022-11-13 12:41:48 -0700

    style and content finish

---
+++ src/components/Skills.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Skill = () => {
+    const style = {
+        width: "22rem"
+    }
+    return (
+        <div className='d-flex flex-wrap justify-content-center'>
+            <div className="card m-3 text-center" style={style}>
+                <div className="card-body">
+                    <h5 className="card-title">Front End</h5>
+                    <div>
+                        <ul className='text-start'>
+                            <li>CSS</li>
+                            <li>React.js</li>
+                            <li>HTML</li>
+                            <li>Handlebars.js</li>
+                            <li>TailWind CSS</li>
+                            <li>Bulma CSS</li>
+                            <li>Bootstrap CSS</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="card m-3 text-center" style={style}>
+                <div className="card-body">
+                    <h5 className="card-title">Back End</h5>
+
+                    <div>
+                        <ul className='text-start'>
+                            <li>Express.js</li>
+                            <li>Express Session</li>
+                            <li>Node.js</li>
+                            <li>RESTful APIs</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="card m-3 text-center" style={style}>
+                <div className="card-body">
+                    <h5 className="card-title">Databases</h5>
+                    <div>
+                        <ul className='text-start'>
+                            <li>GraphQL</li>
+                            <li>MySql</li>
+                            <li>IndexedDB</li>
+                            <li>MongoDB</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="card m-3 text-center" style={style}>
+                <div className="card-body">
+                    <h5 className="card-title">Miscellaneous</h5>
+                    <div>
+                        <ul className='text-start'>
+                            <li>Git</li>
+                            <li>GitHub</li>
+                            <li>Heroku</li>
+                            <li>XMISSION Hosting</li>
+                            <li>Hostgator Hosting</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Skill
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Skills from '../Skills'
 
 const Resume = () => {
   const style = {
     blueText: {
       color: "#2096F3"
+    },
+    goldText: {
+      color: ""
     }
   }
   return (
@@ -12,134 +16,20 @@ const Resume = () => {
         <div className="col">
         </div>
         <div className="col-10 col-sm-12 col-lg-10">
-          <div className='text-white text-center'>
-            <h3>
-              <strong style={style.blueText}>BENJAMIN BUSHMAN</strong>
-            </h3>
-            <p className=''>
-              4049 N. 900 W. Lehi, UT 84043
-            </p>
-            <p>801-372-1410</p>
-            <p>
-              <a className='text-white' href="mailto:benjaminsamuelbushman@gmail.com">benjaminsamuelbushman@gmail.com</a>
-            </p>
-            <p>
-
-            </p>
-            <h1><strong style={style.blueText}>Web Developer</strong></h1>
-            <p>
-              My passion for all things tech has driven me to constantly learn and try new things. I find
-            </p>
-            <p>
-              satisfaction in resolving issues and making IT a smooth experience.  I’m eager to unearth the next game-changing insight for your business.
-            </p>
-            <h3>
-              <strong style={style.blueText}>Employment</strong>
-            </h3>
-            <p>
-              <strong className='text-warning'>TAMS – Lindon, Utah 2016-2017 and 2019-2020</strong>
-            </p>
-            <p>
-              <strong className='text-warning'><em>Computer Repair Technician</em></strong>
-            </p>
-            <ul className='text-start'>
-
-              <li>Full laptop and desktop computer repair and refurbishment with replacement of RAM, graphics cards, processors, fans, heat sinks, motherboards, screens, keyboards, power supplies, cables, BIOS updates, etc.</li>
-
-              <li>Headed a team of computer technicians that processed and repaired tens of thousands of computers.</li>
-
-              <li>HDD and SSD data wipe and erasure with destruction software, Blannco and DBAN.</li>
-
-              <li>Full software diagnostics of hardware components.</li>
-
-              <li>Windows and Linux operating system deployment through Clonezilla and Clone Deploy.
-              </li>
-            </ul>
-            <p>
-              <strong className='text-warning'>Small Business Tech Guys – American Fork, Utah				       2019- Present</strong>
-            </p>
-            <p>
-              <strong className='text-warning'><em>Helpdesk</em></strong>
-            </p>
-            <ul className='text-start'>
-
-              <li>Support 30+ remote employees with all IT needs.</li>
-
-              <li>Monitor computer health, drive health, and performance through RMM software.</li>
-
-              <li>Troubleshoot and support company related software, Fishbowl, QuickBooks, Foxit Phantom, Dropbox, etc.</li>
-
-              <li>Manage all employee accounts
-              </li>
-            </ul>
-            <p>
-              <strong className='text-warning'>Network Providers Inc. – Draper, Utah							2020 – 2021</strong>
-            </p>
-            <p>
-              <strong className='text-warning'><em>Helpdesk</em></strong>
-            </p>
-            <ul className='text-start'>
-
-              <li>Received calls from 70+ clients about issues involving QuickBooks, phones, internet connectivity, printers, etc.</li>
-
-              <li>On call 24/7 to help clients whenever needed.</li>
-
-              <li>Organized onsite visits by consultants when issues couldn’t be resolved remotely.</li>
-
-              <li>Participated in the deployment and use of Remote Monitoring and Management software, including SolarWinds and DATTO RMM.</li>
-
-              <li>Managed clients’ issues through ticketing systems from the mentioned RMM systems.
-              </li>
-            </ul>
-            <p>
-              <strong className='text-warning'>Network Providers Inc. – Draper, Utah						2021 – Present</strong>
-            </p>
-            <p>
-              <strong className='text-warning'><em>Network Administrator</em></strong>
-            </p>
-            <ul className='text-start'>
-
-              <li>Manage 1000+ students and 150+ staff accounts on active directory and group policy on the local and cloud based (Microsoft Azure) domain controller.</li>
-
-              <li>Manage all students and employee email addresses, distribution lists, and groups on the Exchange server.</li>
-
-              <li>Manage the access point controller and deploy wireless access points as well as configure and manage DHCP and DNS.</li>
-
-              <li>Manage FortiGate firewall and smoothwall web filter for safe networking.</li>
-
-              <li>Give support and troubleshoot all employee and student issues ranging from printers to projectors to document cameras.</li>
-
-              <li>Manage all printers at the school as well as the print server.</li>
-
-              <li>Manage the local file server and cloud-based storage.</li>
-
-              <li>Manage 500+ students’ laptops as well as all employee computer hardware.
-              </li>
-            </ul>
-            <p>
-              <strong> </strong>
-            </p>
-            <h3>
-              <strong style={style.blueText}>Education</strong>
-            </h3>
-            <p>
-              High School Graduate | American Heritage School | American Fork, UT | 2013 – 2016
-            </p>
-            <p>
-              Web Development Bootcamp   |   University of Utah	    |     Salt Lake City, UT       |  June 2022 - Nov 2022
-            </p>
-            <h3>
-              <strong style={style.blueText}>Awards/Accomplishments</strong>
-            </h3>
-            <p>
-              - Will receive certification in Full Stack Web Development for the University of Utah
-            </p>
-            <p>
-              - Instrumental in the start of the helpdesk department at Network Providers Inc.
-            </p>
-            <p>
-              References Available upon Request
-            </p>
+          <div>
+            <div className="d-flex justify-content-center">
+              <div className='d-inline-flex'>
+                <h1 style={style.blueText}>Resume</h1>
+              </div>
+            </div>
+            <div>
+              <div className='d-flex justify-content-center flex-wrap'>
+                <Skills />
+              </div>
+              <div className='color-warning'>
+              <a href="https://drive.google.com/file/d/1ibkVEG80J92qcLsPzsud7tZMrz-ey19r/view?usp=share_link" rel="noreferrer" target="_blank" className='text-decoration-none m-3 text-center' style={style.blueText} download><h4 className='text-warning'>Download</h4></a>
+              </div>
+            </div>
           </div>
         </div>
         <div className="col">
