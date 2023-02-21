358e23bbf227e4ea111b623c0b95c8ecae247320
unknown <benbushman98@gmail.com>
2023-02-21 12:55:35 -0700

    added some fixes to issue.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Site from "./components/Site";
+import Style from "./style.css"
 
 const App = () => {
   return (
-    <div>
+    <div style={Style}>
       <Site />
     </div>
   )
