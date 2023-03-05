78913ac5a499ee4c1b1fff20db9c614163ac7fa1
Benjamin Bushman <benbushman98@gmail.com>
2023-03-05 15:05:20 -0700

    added portfolio flip

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,11 +7,14 @@ const Project = () => {
         // Help with mapping idea and implementation. https://www.youtube.com/watch?v=aJgAwjP20RY 
     Projects && Projects.map( project => {
         return (
-            <>
+            <div className='imageContainer'>
             {/* // <div  data-aos={project.aos} className="card m-3 text-center" key={project.id} style={{width:"18rem", background:"#d8d8d8",}} > */}
                 {/* <div className="card-body"> */}
                     {/* <h5 className="card-title text-decoration-underline">{project.title}</h5> */}
-                    <img src={project.src} className="rounded-4 m-4" alt={project.alt} style={{height: "200px", width: "350px"}}></img>
+                    <img src={project.src} data-aos={project.aos} className="rounded-4 m-4 imageFlip" alt={project.alt} style={{height: "200px", width: "350px"}}></img>
+                    <div className='imageOverlay'>
+                        <div className='test'>TEST</div>
+                    </div>
                     {/* <p className="card-text py-2">{project.description}</p> */}
                 {/* </div> */}
 
@@ -21,7 +24,7 @@ const Project = () => {
                         {/* Repo</a> */}
                 {/* </div> */}
             {/* // </div> */}
-            </>
+            </div>
         )
     })
     )
